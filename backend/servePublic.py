3ecb4768d3cd506efbe61f6e7ef8ab85facf61ca
MladoniSzabi <szabimlado@gmail.com>
2022-06-19 13:31:17 +0100

    removed print from public controller.

--- backend/servePublic.py
+++ backend/servePublic.py
@@ -3,8 +3,6 @@ from flask import send_file, send_from_directory
 def setUpRoutes(app):
     @app.route("/static/<subpath>")
     def static_files(subpath = "index.html"):
-        print(subpath)
-        print(2)
         return send_from_directory("dist", subpath)
     
     @app.route("/favicon.ico")
@@ -15,5 +13,4 @@ def setUpRoutes(app):
     @app.route("/<subpath>")
     @app.route("/<path:subpath>")
     def index(subpath = ""):
-        print(1)
         return send_file("dist/index.html")
