ba536164c99fe588114111b9181d00f5f5fcecf5
MladoniSzabi <szabimlado@gmail.com>
2022-07-10 13:57:45 +0100

    created functions on the backend to view, edit and create keybindings to execute written functions.

--- backend/serveProjectAPI.py
+++ backend/serveProjectAPI.py
@@ -1,3 +1,5 @@
+from cgitb import reset
+from crypt import methods
 from flask import request, make_response
 import os
 import json
@@ -41,3 +43,56 @@ def setUpRoutes(app):
         with open("projects/" + project + "/files/" + filepath, "w") as f:
             f.write(request.get_data(as_text=True))
             return ""
+    
+    @app.route("/api/project/<project>/keybindings", methods=["GET"])
+    def getKeybindings(project):
+        pathToMacros = "projects/" + project + "/keybindings.json"
+        if not os.path.exists(pathToMacros):
+            return []
+        with open(pathToMacros) as f:
+            return f.read()
+    
+    @app.route("/api/project/<project>/keybindings", methods=["PUT"])
+    def createKeybindings(project):
+        pathToMacros = "projects/" + project + "/keybindings.json"
+        data = request.json() or request.form
+
+        with open(pathToMacros, "rw") as f:
+            if os.path.exists(pathToMacros):
+                existingMacros = json.loads(f.read())
+            else:
+                existingMacros = []
+            existingMacros.append({
+                "keybinding": data["keybinding"],
+                "path": data["path"]
+            })
+            f.write(json.dumps(existingMacros))
+        return ""
+    
+    @app.route("/api/project/<project>/keybindings", methods=["PUT"])
+    def editKeybindings(project):
+        pathToMacros = "projects/" + project + "/keybindings.json"
+        data = request.json() or request.form
+
+        if not os.path.exists(pathToMacros):
+            response = make_response({"error": "Keybinding does not exist"})
+            response.status_code = 404
+            return response
+        
+        with open(pathToMacros, "rw") as f:
+            existingMacros = json.loads(f.read())
+            
+            if data["index"] >= len(existingMacros) or data["index"] < 0:
+                response = make_response({"error": "Keybinding does not exist"})
+                response.status_code = 404
+                return response
+
+            existingMacros[int(data["index"])] = {
+                "keybinding": data["keybinding"],
+                "path": data["path"]
+            }
+
+            f.write(json.dumps(existingMacros))
+
+        return ""
+    
