1df3f2e1e89200241deae2ffccefb78cb2c6f169
MladoniSzabi <szabimlado@gmail.com>
2022-08-22 18:13:00 +0100

    WIP: adding ability to get image of specific window.

---
+++ backend/window.py
@@ -0,0 +1,41 @@
+import sys
+
+def windows(windowName):
+    from win32gui import FindWindow, GetWindowRect
+    window_handle = FindWindow(None, windowName)
+    window_rect = GetWindowRect(window_handle)
+    x = window_rect[0]
+    y = window_rect[1]
+    w = window_rect[2] - x
+    h = window_rect[3] - y
+    return (x, y, w, h)
+
+def linux(windowName):
+    from gi.repository import Gtk, Wnck
+
+    Gtk.init([])  # necessary only if not using a Gtk.main() loop
+    screen = Wnck.Screen.get_default()
+    screen.force_update()  # recommended per Wnck documentation
+
+    # loop all windows
+    targetWindow = None
+    for window in screen.get_windows():
+        if window.get_name() == windowName:
+            targetWindow = window
+
+    dimensions = targetWindow.get_geometry()
+
+    # clean up Wnck (saves resources, check documentation)
+    window = None
+    targetWindow
+    screen = None
+    Wnck.shutdown()
+    return dimensions
+
+def getWindow(windowName):
+    if sys.platform == 'window':
+        return windows(windowName)
+    elif sys.platform == 'linux':
+        return linux(windowName)
+    else:
+        raise "getWindow() not implemented for your platform"
