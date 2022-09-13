a0e616c2c8f1596e638cc6497cab34e997bf7e8e
MladoniSzabi <szabimlado@gmail.com>
2022-09-13 12:53:12 +0100

    made the images the language uses be references.

--- backend/interpreter/image_processing.py
+++ backend/interpreter/image_processing.py
@@ -1,3 +1,4 @@
+from json import load
 import pyautogui
 import cv2
 import numpy as np
@@ -5,12 +6,26 @@ import pytesseract
 import os
 from . import  builtin_funcs
 
+loadedImages = {}
+
+def createNewImage(img, context):
+    if context["projectName"] not in loadedImages:
+        loadedImages[context["projectName"]] = []
+    
+    imgId = len(loadedImages[context["projectName"]])
+    loadedImages[context["projectName"]].append(img)
+    return {"type": "img", "id": imgId, "project": context["projectName"]}
+
 def printImage(image, context=None):
     imageid = os.listdir("printedimages")
     imageName = "printedimages/" + str(len(imageid)) + ".png"
+    image = loadedImages[image["projectName"]][image["id"]]
     cv2.imwrite(imageName, image)
     builtin_funcs.onPrint({"type": "image", "message": imageName})
     return imageName
+
+def cloneImage(img, context):
+    return createNewImage(loadedImages[img["projectName"]][img["id"]], context)
 
 def takeScreenshot(region=None, context=None):
     scrshot = None
@@ -18,8 +33,8 @@ def takeScreenshot(region=None, context=None):
         scrshot = pyautogui.screenshot(region=region)
     else:
         scrshot = pyautogui.screenshot()
-    return cv2.cvtColor(np.array(scrshot),
-                     cv2.COLOR_RGB2BGR)
+    return createNewImage(cv2.cvtColor(np.array(scrshot),
+                     cv2.COLOR_RGB2BGR), context)
 
 def saveImage(filename, img, context):
     pathToImageFolder=None
@@ -36,7 +51,7 @@ def saveImage(filename, img, context):
             pathToImage = os.path.join(pathToImageFolder, os.path.split(filename)[1])
         
     
-    cv2.imwrite(pathToImage, img)
+    cv2.imwrite(pathToImage, loadedImages[img["projectName"]][img["id"]])
 
 def locateCenterOnScreen(imageName, confidence=0.9, context=None):
     pathToImage = os.path.join("projects", context["projectName"], "images", imageName)
@@ -47,29 +62,31 @@ def locateCenterOnScreen(imageName, confidence=0.9, context=None):
 
 def loadImage(filename, context):
     pathToImage = os.path.join("projects", context["projectName"], "images", filename)
-    return cv2.imread(pathToImage, cv2.IMREAD_COLOR)
+    return createNewImage(cv2.imread(pathToImage, cv2.IMREAD_COLOR), context)
 
 def imageToText(img, context=None):
-    return pytesseract.image_to_string(img)
+    return pytesseract.image_to_string(loadedImages[img["projectName"]][img["id"]])
 
 def imageToBlackAndWhite(img, context=None):
-    gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    return cv2.threshold(gray_image, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+    gray_image = cv2.cvtColor(loadedImages[img["projectName"]][img["id"]], cv2.COLOR_BGR2GRAY)
+    loadedImages[img["projectName"]][img["id"]] = cv2.threshold(gray_image, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+    return img
 
 def blurImage(img, blurSize=3, context=None):
-    return cv2.GaussianBlur(img,(blurSize,blurSize),cv2.BORDER_DEFAULT)
+    loadedImages[img["projectName"]][img["id"]] = cv2.GaussianBlur(loadedImages[img["projectName"]][img["id"]],(blurSize,blurSize),cv2.BORDER_DEFAULT)
+    return img
 
 def getAveragePixel(img, context=None):
-    return cv2.meanStdDev(img)[0]
+    return cv2.meanStdDev(loadedImages[img["projectName"]][img["id"]])[0]
 
 def getImageDimensions(img, context=None):
-    return [img.shape[1], img.shape[0]]
+    return [loadedImages[img["projectName"]][img["id"]].shape[1], loadedImages[img["projectName"]][img["id"]].shape[0]]
 
 def compareImages(img1, img2, context=None):
-    if(img1.shape != img2.shape):
+    if(loadedImages[img1["projectName"]][img1["id"]].shape != loadedImages[img2["projectName"]][img2["id"]].shape):
         return False
     
-    difference = cv2.subtract(img1, img2)
+    difference = cv2.subtract(loadedImages[img1["projectName"]][img1["id"]], loadedImages[img2["projectName"]][img2["id"]])
     b, g, r = cv2.split(difference)
     if \
         cv2.countNonZero(b) == 0 and \
