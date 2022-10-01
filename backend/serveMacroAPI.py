b95cb8877e4c7291143ac5557c6aafa5c0ad045f
MladoniSzabi <szabimlado@gmail.com>
2022-10-01 15:31:41 +0100

    removed unused endpoints.

--- backend/serveMacroAPI.py
+++ backend/serveMacroAPI.py
@@ -4,14 +4,6 @@ from interpreter import builtin_funcs
 import time
 
 def setUpRoutes(app: Flask):
-    app.add_url_rule("/api/mouse/move/<int:x>/<int:y>", "moveMouse", builtin_funcs.moveMouse)
-    app.add_url_rule("/api/keyboard/press/<c>", "pressKey", builtin_funcs.pressKey)
-    app.add_url_rule("/api/keyboard/release/<c>", "releaseKey", builtin_funcs.releaseKey)
-    app.add_url_rule("/api/keyboard/tap/<c>", "tapKey", builtin_funcs.tapKey)
-    app.add_url_rule("/api/record/start", "startRecording", builtin_funcs.startRecording, methods=["POST"])
-    app.add_url_rule("/api/record/stop", "stopRecording", builtin_funcs.stopRecording)
-    app.add_url_rule("/api/wait/<float:secs>", "sleep", time.sleep)
-
     @app.route("/api/record/stop/hotkey/<hotkey>", methods=["POST"])
     def setStopRecordingButton(hotkey):
         settingsPath = "projects/" + request.form["projectName"] + "/settings.json"
